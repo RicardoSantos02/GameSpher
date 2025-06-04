03383d7fa1571c983f74e7e6cde7317c3876b71a
Ricardo Santos <153729295+RicardoSantos02@users.noreply.github.com>
2025-06-04 17:34:38 -0300

    Atualizar o Application.java

--- app/src/main/java/br/edu/ifrn/gamespher/Application.java
+++ app/src/main/java/br/edu/ifrn/gamespher/Application.java
@@ -1,12 +1,13 @@
-package br.edu.br.ifrn.gamespher;
+package br.edu.ifrn.gamespher;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
 
-    public static void main(String[] args) {
-        SpringApplication.run(primarySource:Application.class,args)
-    }
+}
 
 }
