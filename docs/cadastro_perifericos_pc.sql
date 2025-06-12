4b1d693c0e9fe20f8985ddb4874a904b856b6129
Ricardo Santos <153729295+RicardoSantos02@users.noreply.github.com>
2025-06-12 14:26:44 -0300

    indentado

--- docs/cadastro_perifericos_pc.sql
+++ docs/cadastro_perifericos_pc.sql
@@ -2,7 +2,9 @@
 CREATE TABLE perifericos_pc (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    categoria VARCHAR(50) CHECK (categoria IN ('Teclado', 'Mouse', 'Cadeira Gamer', 'Outro')),
+    categoria VARCHAR(50) CHECK (
+        categoria IN ('Teclado', 'Mouse', 'Cadeira Gamer', 'Outro')
+    ),
     marca VARCHAR(50),
     preco DECIMAL(10, 2) NOT NULL
 );
